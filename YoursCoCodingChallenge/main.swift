611325340ac740748734c011dc5cc2dc8dc2b5d0
Jeff Nuss <jeffnuss+git.commits@gmail.com>
2017-11-09 21:34:35 -0700

    Initial Commit

---
+++ YoursCoCodingChallenge/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  YoursCoCodingChallenge
+//
+//  Created by Jeff Nuss on 11/9/17.
+//  Copyright © 2017 Jeff Nuss. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
